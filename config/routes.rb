0015a9003bc321196a9703e585b3061e55fd17fe
rodparejap <91509840+thecutcatcompany@users.noreply.github.com>
2021-10-15 02:04:08 +0000

    avances

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  
+  resources :productos do
+    collection do
+      get :filter
+    end
+  end
+  resources :categoria
   devise_for :users
   resources :proyectos
   root "landpage#index"
+
   get 'landpage/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
